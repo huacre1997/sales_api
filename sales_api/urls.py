97880a7ec30a837fec182f19c0a94a862ddb6510
mr01robot <toshiro.kenny@gmail.com>
2022-10-18 18:46:13 -0500

    Se actualizó los datos de la urls

--- sales_api/urls.py
+++ sales_api/urls.py
@@ -19,7 +19,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/warehouse/', include("apps.warehouse.api.urls")),
 
 ]
 if settings.DEBUG and "debug_toolbar" in settings.INSTALLED_APPS:
