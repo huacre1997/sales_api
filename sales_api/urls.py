37cb5ef90ed7c5652028ef0ec5fa12d7c97010da
Joel Huacre Enciso <huacreenciso97@gmail.com>
2022-10-21 00:15:06 -0500

    Installing Swagger

--- sales_api/urls.py
+++ sales_api/urls.py
@@ -14,17 +14,43 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
+# Swagger -> https://drf-yasg.readthedocs.io/en/stable/
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+# repath -> Soporta url's con formato regex
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/warehouse/', include("apps.warehouse.api.urls")),
     path('api/crm/', include("apps.crm.api.urls")),
-    path('api/sales/', include("apps.sales.api.urls"))
+    path('api/sales/', include("apps.sales.api.urls")),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger',
+            cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc',
+            cache_timeout=0), name='schema-redoc'),
 
 ]
+
+# Importa el debug toolbar solo si estamos en modo DEBUG=True
 if settings.DEBUG and "debug_toolbar" in settings.INSTALLED_APPS:
     import debug_toolbar
 
